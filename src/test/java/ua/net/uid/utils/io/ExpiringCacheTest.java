c1c27c58e377c89148869055f1e446fe245351f3
Vladimir Burdo <nightfall@uid.net.ua>
2020-05-15 18:16:45 +0300

    fix warning

--- src/test/java/ua/net/uid/utils/io/ExpiringCacheTest.java
+++ src/test/java/ua/net/uid/utils/io/ExpiringCacheTest.java
@@ -17,7 +17,7 @@ package ua.net.uid.utils.io;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 //TODO ExpiringCacheTest
 class ExpiringCacheTest {
